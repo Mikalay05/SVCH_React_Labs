bf6a518c3a55b3325b49de172e4ad3c9bdc1a1ce
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-10-23 09:26:26 +0300

    feat: add NavSection component

--- src/Components/NavSection/NavSection.jsx
+++ src/Components/NavSection/NavSection.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
+import './NavSection.css'
+
+// import Data from '../../Json/linksInHeaders.json'
+
 export default function NavSection() {
+
+    const data  = [
+        {
+            nameLinks: 'Sign In',
+            pathLink: '/authorization'
+        },
+        {
+            nameLinks: 'Product Support',
+            pathLink: '/product-support'
+        },
+        {
+            nameLinks: 'Company',
+            pathLink: '/company'
+        },        {
+            nameLinks: 'Contact us',
+            pathLink: '/contact-us'
+        },
+    ]
+
+    const resultLinks = data.map((element, index) => {
+        return (
+            <a key={index} className="stylesLink" href={element.pathLink}>{element.nameLinks}</a>
+        )
+    })
+
     return (
-        <nav>
-            
+        <nav className="content-links"> 
+            {resultLinks}
         </nav>
     )
 }
