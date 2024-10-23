e93d82c89462dbf71187ae677eff83135836d562
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-10-23 09:31:32 +0300

    feat: getting a links from a json file

--- src/Components/NavSection/NavSection.jsx
+++ src/Components/NavSection/NavSection.jsx
@@ -2,29 +2,11 @@ import React from "react";
 
 import './NavSection.css'
 
-// import Data from '../../Json/linksInHeaders.json'
+import Data from '../../Json/linksInHeaders.json'
 
 export default function NavSection() {
 
-    const data  = [
-        {
-            nameLinks: 'Sign In',
-            pathLink: '/authorization'
-        },
-        {
-            nameLinks: 'Product Support',
-            pathLink: '/product-support'
-        },
-        {
-            nameLinks: 'Company',
-            pathLink: '/company'
-        },        {
-            nameLinks: 'Contact us',
-            pathLink: '/contact-us'
-        },
-    ]
-
-    const resultLinks = data.map((element, index) => {
+    const resultLinks = Data.map((element, index) => {
         return (
             <a key={index} className="stylesLink" href={element.pathLink}>{element.nameLinks}</a>
         )
